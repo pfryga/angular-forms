9cdace36d278e0974b23d270432fbc8f0f044347
Piotr Fryga <piotr.fryga@allegro.pl>
2015-03-17 20:26:55 +0100

    fix

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# Angular JS forms validation
+# Validation
+Forms validation with AngularJS
+
+version: 1.0.0
 
 ## Installation
 
-`
+```
 bower install
-`
+```
+
+## License
+
+Released under the MIT License
+
---
+++ app/script/app.js
@@ -0,0 +1,11 @@
+var validationApp = angular.module('validationApp', []);
+
+validationApp.controller('mainController', function($scope) {
+
+    $scope.submitForm = function(isValid) {
+        if (isValid) {
+            console.log('our form is amazing');
+        }
+    };
+
+});
